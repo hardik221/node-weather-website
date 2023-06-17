e6b0f8c9ca20988accd57db25e309b4d0bf02e5b
Hardik Amareliya <hardikamareliya221@gmail.com>
2023-06-17 16:29:15 -0400

    Add feels like and humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Location does not exist for given latitude and longitude', undefined);
         } else{
-            callback(undefined, `${body.current.weather_descriptions[0]}: It is currently ${body.current.temperature} degrees outside. There is a ${body.current.precip}% chance of rain.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}: It is currently ${body.current.temperature} degrees outside but it feels like ${body.current.feelslike}. There is a ${body.current.precip}% chance of rain. The humidity is ${body.current.humidity}.`)
         }
     })
 }
